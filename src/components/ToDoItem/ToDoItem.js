0643ff2ca491b1faf638d30362e0d15b355f6672
Jacob Zavita <jacobzavita@gmail.com>
2021-08-20 16:21:40 -0700

    completed/checkmark

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -5,10 +5,21 @@ const ToDoItem = props => {
     props.setTodos(props.todos.filter(el => el.id !== props.todo.id))
   }
 
+  const handleComplete = () => {
+    props.setTodos(props.todos.map(item => {
+      if(item.id === props.todo.id){
+        return{
+          ...item, completed: !item.completed
+        }
+      }
+      return item
+    }))
+  }
+
   return(
     <div className="todo">
-      <li className="todo-item">{props.text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${props.todo.completed ? "completed" : ''}`}>{props.text}</li>
+      <button onClick={handleComplete} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={handleDelete} className="trash-btn">
